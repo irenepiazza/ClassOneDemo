ce0319febabb6c18559f828fa49d07530c40a33d
irenepiazza <46923298+irenepiazza@users.noreply.github.com>
2019-01-22 11:29:17 +0100

    initial upload

---
+++ p5/project one demo/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  // put setup code here
+createCanvas(400,400);
+}
+
+function draw() {
+  // put drawing code here
+stroke(255);
+fill(255);
+background(0);
+text("hello world",20,height/2);
+}
